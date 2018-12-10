589571c9b45f3f4eab2dd9f7dfd1cd10babf06a7
Liam Cole <liam@liamiam.com>
2018-12-10 15:46:32 -0800

    LSH Command Line

--- kernel.cpp
+++ kernel.cpp
@@ -2,18 +2,39 @@
 #include <stdlib.h>
 #include <string>
 
-void error() {
-  system("play sound.wav");
-  exit(1);
+void print(const char *text) {
+  printf(text);
+  printf("\n");
+} 
+
+void error(int a) {
+  system("play sound.wav -q");
+  exit(a);
 }
 
-void print(char *text) {
-  printf(strcat(text, "\n"));
-} 
+void non_fatal_error(const char *reason) {
+  system("play sound.wav -q");
+  print(reason);
+}
+
+char* cmdGET(const char *prompt) {
+  printf(prompt);
+  char *input;
+  scanf("%c", input);
+  return input;
+}
+
+
 
 int main(int argc, char const *argv[])
 {
   print("[LSH] Kernel Begin");
-  
+  // Expansion code goes here...
+  print("[LSH] Starting loop");
+  while(true) {
+    char *a = cmdGET("LSH> ");
+    if (fopen(a, "r")) system(a);
+    else non_fatal_error("Invalid Command");
+  }
   return 0;
 }
